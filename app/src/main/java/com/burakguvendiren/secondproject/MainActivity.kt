db9be393660ecc734889c7c7231116d22ca28db5
Burak Güvendiren <guvendirenn@gmail.com>
2023-01-05 23:23:21 +0300

    Learned Functions

--- app/src/main/java/com/burakguvendiren/secondproject/MainActivity.kt
+++ app/src/main/java/com/burakguvendiren/secondproject/MainActivity.kt
@@ -7,5 +7,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       var j = 0
+        if (j == 0) {
+            ilkFonksiyon()
+        } else {
+            ikinciFonksiyon( )
+        }
+
+    }
+
+    fun ilkFonksiyon() {
+        println("ilk fonksiyon çalıştırıldı")
+    }
+
+    fun ikinciFonksiyon() {
+        println("ikinci fonksiyon çalıştırıldı")
     }
 }
