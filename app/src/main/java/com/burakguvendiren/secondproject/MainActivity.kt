320f573552de8daadd49674aa23bee90303b9f46
Burak Güvendiren <guvendirenn@gmail.com>
2023-01-06 21:27:16 +0300

    Learned Parameters2

--- app/src/main/java/com/burakguvendiren/secondproject/MainActivity.kt
+++ app/src/main/java/com/burakguvendiren/secondproject/MainActivity.kt
@@ -3,36 +3,37 @@ package com.burakguvendiren.secondproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var j = -10
+        var j = 0
+
         if (j == 0) {
             ilkFonksiyon()
-        } else {
+            }
+            else {
             ikinciFonksiyon()
         }
-
         cikarma(x = 100, y = 20)
+        val textView: TextView = findViewById(R.id.textView)
 
     }
 
     fun ilkFonksiyon() {
         println("ilk fonksiyon çalıştırıldı")
     }
-
     fun ikinciFonksiyon() {
         println("ikinci fonksiyon çalıştırıldı")
     }
-
         //Girdi Almak
 
-        fun cikarma(x: Int, y:Int) {
-            textView.text = "Sonuç: ${x-y}"
-        }
+    fun cikarma(x: Int, y:Int){
+        textView.text = "Sonuç: ${x-y}"
 
 
     }
